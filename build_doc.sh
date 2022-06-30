b75f8ec7f0e7364091fc8239a90541c1145dcac1
ZeeWanderer <mxx888777@gmail.com>
2022-06-30 04:27:17 -0700

    [doc] try fix

--- build_doc.sh
+++ build_doc.sh
@@ -1,3 +1,4 @@
+xcodebuild build -scheme Tutorials -destination generic/platform=iOS
 xcodebuild docbuild -scheme Tutorials \
 -destination generic/platform=iOS \
-OTHER_DOCC_FLAGS="--transform-for-static-hosting --output-path docs --hosting-base-path tutorials"
+OTHER_DOCC_FLAGS="--transform-for-static-hosting --output-path docs --hosting-base-path SwiftUITutorials"
