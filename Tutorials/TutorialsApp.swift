2c2eb632ce08c764ed567857b8549499ec5c504b
ZeeWanderer <mxx888777@gmail.com>
2022-06-30 03:33:34 -0700

    Initial Commit

---
+++ Tutorials/TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialsApp.swift
+//  Tutorials
+//
+//  Created by Maksym Kulyk on 30.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
