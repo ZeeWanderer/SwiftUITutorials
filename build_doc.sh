654aafcf6ef9a0d9923f25abb4f029f51ca44b5e
ZeeWanderer <mxx888777@gmail.com>
2022-06-30 04:56:20 -0700

    [doc] final

--- build_doc.sh
+++ build_doc.sh
@@ -1,4 +1,4 @@
-xcodebuild build -scheme Tutorials -destination generic/platform=iOS
+#xcodebuild build -scheme Tutorials -destination generic/platform=iOS
 xcodebuild docbuild -scheme Tutorials \
 -destination generic/platform=iOS \
 OTHER_DOCC_FLAGS="--transform-for-static-hosting --output-path docs --hosting-base-path SwiftUITutorials"
